562a2ebe7dc8eff1f2173137b78399e9e61c2241
Leslie Hochsztein <lhochsz@gmail.com>
2016-10-01 15:49:36 -0400

    Added static pages, navbar, and flash messages.

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,9 @@ module Park4
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+      config.generators do |g|
+      g.orm             :active_record
+      g.template_engine :erb
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'static_pages#home'
+  get 'home', to: 'static_pages#home', as: 'home'
+  get 'about', to: 'static_pages#about', as: 'about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
