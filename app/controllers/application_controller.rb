91261fa199e4ef9272810a3d85b1b4dedba3ab43
Leslie Hochsztein <lhochsz@gmail.com>
2016-10-03 14:51:41 -0400

    Updated configuration for username so can edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
     devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
 
 end
