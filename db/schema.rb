eb8c2fcd0ec8bebe723f7d883f0075dab4b190f6
Leslie Hochsztein <lhochsz@gmail.com>
2016-10-03 13:36:11 -0400

    Working on username

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161003145352) do
+ActiveRecord::Schema.define(version: 20161003165643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,13 +26,6 @@ ActiveRecord::Schema.define(version: 20161003145352) do
     t.text     "description"
     t.string   "title"
     t.index ["user_id"], name: "index_spots_on_user_id", using: :btree
-  end
-
-  create_table "todos", force: :cascade do |t|
-    t.string   "title",      null: false
-    t.boolean  "completed",  null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
